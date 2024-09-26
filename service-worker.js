a3842a60d209be590ad6bc551b0d0d3d8eb5e7db
JibbelJabbel <albedania@gmail.com>
2024-09-26 15:37:11 +0200

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -1,23 +1,21 @@
-self.addEventListener('install', (event) => {
-  event.waitUntil(
-    caches.open('calculator-cache').then((cache) => {
-      return cache.addAll([
-        '/',
-        '/index.html',
-        '/style.css',
-        '/app.js',
-        '/manifest.json',
-        '/images/icon-192x192.png',
-        '/images/icon-512x512.png'
-      ]);
-    })
-  );
+self.addEventListener('install', function(event) {
+    event.waitUntil(
+        caches.open('pwa-cache').then(function(cache) {
+            return cache.addAll([
+                '/',
+                '/index.html',
+                '/style.css',
+                '/app.js',
+                '/manifest.json'
+            ]);
+        })
+    );
 });
 
-self.addEventListener('fetch', (event) => {
-  event.respondWith(
-    caches.match(event.request).then((response) => {
-      return response || fetch(event.request);
-    })
-  );
+self.addEventListener('fetch', function(event) {
+    event.respondWith(
+        caches.match(event.request).then(function(response) {
+            return response || fetch(event.request);
+        })
+    );
 });
