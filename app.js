b33b691a9d62ef90ffc001176cba3b38442d3ee6
JibbelJabbel <albedania@gmail.com>
2024-09-26 16:03:31 +0200

    Update app.js

--- app.js
+++ app.js
@@ -13,9 +13,9 @@ function calculateSum() {
 if ('serviceWorker' in navigator) {
     window.addEventListener('load', function() {
         navigator.serviceWorker.register('/service-worker.js').then(function(registration) {
-            console.log('ServiceWorker registration successful with scope: ', registration.scope);
+            console.log('Service Worker registered with scope: ', registration.scope);
         }, function(err) {
-            console.log('ServiceWorker registration failed: ', err);
+            console.log('Service Worker registration failed: ', err);
         });
     });
 }
