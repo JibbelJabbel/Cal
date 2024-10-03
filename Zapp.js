48bfd3d8be05adf5007a1358f744d90dff254be1
JibbelJabbel <albedania@gmail.com>
2024-10-03 14:00:33 +0200

    Create Zapp.js

---
+++ Zapp.js
@@ -0,0 +1,23 @@
+function calculateSum() {
+    const disp = parseFloat(document.getElementById('disp').value) || 0;
+    const meas = parseFloat(document.getElementById('meas').value) || 0;
+    const Z = parseFloat(document.getElementById('Z').value) || 0;
+
+    const sum = Z + disp - meas;
+    document.getElementById('result').textContent = "New Z: " + sum;
+}
+
+function goBack() {
+            window.history.back();
+}
+
+// Register the service worker
+if ('serviceWorker' in navigator) {
+    window.addEventListener('load', function() {
+        navigator.serviceWorker.register('service-worker.js').then(function(registration) {
+            console.log('Service Worker registered with scope: ', registration.scope);
+        }, function(err) {
+            console.log('Service Worker registration failed: ', err);
+        });
+    });
+}
